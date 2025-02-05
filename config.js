786382415738e976a186af8bc80d1862c875dadf
PiyushXo <139626103+PiyushXo@users.noreply.github.com>
2025-02-05 22:32:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    pageTitle: "Boobooo",
+    popupTitle: '😛😛😛',
+    description: `Well since you have already clicked on this, i gotta ask you something. It's kinda important.`,
+    introButton: 'Click Me Bish!',
+    title: 'Will You Be My Valentine Forever?',
+    yesBtn: 'Yess 😍',
+    noBtn: 'No 😠',
+    question: `Okay i knew you'd say yes hehehe 💋💋 \n and now you gotta tell me why do you love me ?`,
+    sendBtn: 'Kutta!',
+    reply: 'Cause you\'re my jaan baby! You\'re so sexy, funny and smart. You\'re the best thing that\'s ever happened to me. My ex hoes are so jealous cause of you..I love you so much my bubu!!😘😘😘',
+    messPop: 'staaap i\'m blushing rn! Fine, i\'ll be pounding you tonight!',
+    messDesc: 'Alright now you\'re stuck with me for life so get used to this stupid.. ',
+    btnAccept: 'YESS DADDYYY!!❤️'
+}
